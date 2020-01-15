ea1ae209c2a5601d3c7dced1386f9ac902372435
Julien Capellari <julien@capellari.net>
2020-01-15 18:01:41 +0100

    add: security middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response, Router } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 import setup_users from './routes/users';
 
 // Prepare express app
 export const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
