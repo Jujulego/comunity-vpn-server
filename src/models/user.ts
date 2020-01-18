a2ef2d1b1e6bfb8e874e9875439cb104af9a9d26
Julien Capellari <julien@capellari.net>
2020-01-18 23:39:25 +0100

    update: never send password or tokens fields

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,7 @@
 import mongoose, { Model, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
 import validator from 'validator';
+import _ from 'lodash';
 
 import { User as UserData } from 'data/user';
 import { generateToken } from 'data/token';
@@ -39,6 +40,10 @@ schema.methods.generateAuthToken = async function() {
   await this.save();
 
   return token;
+};
+
+schema.methods.toJSON = function(options) {
+  return _.omit(this.toObject(options), ['password', 'tokens']);
 };
 
 // Statics
